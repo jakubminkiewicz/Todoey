03c84e8d5fd7493e91dc30dea9945cf4bb98a02e
jakub <jakubcontact@icloud.com>
2018-07-19 16:03:40 +0200

    Added local storage

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,8 +12,14 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["Do bed", "Make memes", "Shout at clouds"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         
     }
     
@@ -51,6 +57,7 @@ class TodoListViewController: UITableViewController {
             //what will happen once the user clicks the add item button on our UIAlert
             
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
             }
         
